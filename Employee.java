a788611bfb2ff2fc2f7008f0d535ec599a0ff7ca
sunder1999-v <104760204+sunder1999-v@users.noreply.github.com>
2022-05-02 14:48:25 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,6 +1,54 @@
-public class Employee
+import java.util.*;
+
+class Test
 {
- private int empno;
- private String ename;
- private double salary;
- public
+
+ int empid;
+ String ename;
+ double salary;
+ int deptno;
+
+  if(deptno=10){
+  salary=sal*10/100+sal
+  System.out.println(salary);
+  }
+  elseif(deptno=20){
+  salary=sal*15/100+sal
+  System.out.println(salary);
+  }
+   elseif(deptno=30){
+   salary=sal*20/100+sal
+   System.out.println(salary);
+   }
+  else{
+   System.out.println("invalid Deptno");
+  }
+
+
+}
+
+
+
+
+class Employee
+{
+ public static void main(String[] args)
+ {
+  
+  Test a1=new Test();
+  Scanner scanner = new Scanner(System.in);
+  System.out.println("enter employee id:");
+  a1.empid=scanner.nextInt();
+  System.out.println("enter employee name:");
+  a1.ename=scanner.nextInt();
+  System.out.println("enter employee salary:");
+  a1.salary=scanner.nextInt();
+  System.out.println("enter dept no:");
+  a1.deptno=scanner.nextInt();
+
+
+  System.out.println("hike increment is:" +a1.salary);
+
+}
+
+ 
